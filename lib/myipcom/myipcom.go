22682a3916aae178b5c02bda00ff13449fe21eaf
Ben Garrett <code.by.ben@gmail.com>
2021-04-12 22:26:51 +1000

    Fixed --first triggering context.Canceled errors.

--- lib/myipcom/myipcom.go
+++ lib/myipcom/myipcom.go
@@ -85,6 +85,9 @@ func Request(ctx context.Context, cancel context.CancelFunc, url string) (string
 			fmt.Printf("\n%s: timeout", domain)
 			return "", nil
 		default:
+			if ctx.Err() == context.Canceled {
+				return "", nil
+			}
 			return "", fmt.Errorf("%s error: %s", domain, err)
 		}
 	}
